583b8f7ad1b02396459a21fa9222d9243fa0a5ba
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-06-30 14:55:15 +0800

    Update autodumpdata.py

--- scripts/schedules/autodumpdata.py
+++ scripts/schedules/autodumpdata.py
@@ -16,14 +16,15 @@ REPO_ROOT = d['REPO_ROOT']
 def dumpdata_from_DB():
     datenow = datetime.now().strftime("%Y%m%d%H%M")
     backup_cmd = f"{PYTHONPATH_ABS} {REPO_ROOT}\\manage.py dumpdatautf8 --output=dumpdata\db.{datenow}.json"
+
+    os.chdir(REPO_ROOT)
+    print(backup_cmd)
     print('start...')
     os.system(backup_cmd)
 
 
-# schedule.every().day.at('09:00').do(dumpdata_from_DB)
+schedule.every().day.at('03:00').do(dumpdata_from_DB)
 
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
-
-dumpdata_from_DB()
+while True:
+    schedule.run_pending()
+    time.sleep(60)
