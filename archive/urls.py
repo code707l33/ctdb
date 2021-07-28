88405649332deb835580bcccdca30d178363b2b9
ChrisXiao-4064 <4064w007@email.ntou.edu.tw>
2021-07-28 14:01:50 +0800

    create journals area

--- archive/urls.py
+++ archive/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from .views import archive_create, archive_delete, archive_list, archive_update
+# from .views import journals_list, journals_create, journals_update, journals_delete
 
 app_name = 'archive'
 
@@ -9,4 +10,8 @@ urlpatterns = [
     path('archives/add/', archive_create, name='archive_create'),
     path('archives/<int:pk>/change/', archive_update, name='archive_update'),
     path('archives/<int:pk>/delete/', archive_delete, name='archive_delete'),
+    # path('archives/', journals_list, name='journals_list'),
+    # path('archives/add/', journals_create, name='journals_create'),
+    # path('archives/<int:pk>/change/', journals_update, name='journals_update'),
+    # path('archives/<int:pk>/delete/', journals_delete, name='journals_delete'),
 ]
